418e41cfade7c95a3359e66cb3c44c91a48f6fe4
Shahar Melamed <shaharonnn@gmail.com>
2021-03-25 21:17:32 +0200

    Added root site

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import http
 
+import jinja2
 from flask import Flask, jsonify
 
 import domain_finder
@@ -8,11 +11,32 @@ app = Flask(__name__)
 
 
 @app.route('/api/domain', methods=["GET"])
-def hello_world():
+def api_get_domain():
+    """
+    Get a non-existing domain.
+
+    :return: The result JSON and the status code.
+    """
     return jsonify({
         "url": domain_finder.find_domain(),
         "status": http.HTTPStatus.OK,
     }), http.HTTPStatus.OK
+
+
+@app.route('/', methods=["GET"])
+def root_page():
+    """
+    Get the root page.
+
+    :return: The content of the page and the status code.
+    """
+    environment = jinja2.Environment(
+        loader=jinja2.FileSystemLoader("templates"),
+        autoescape=jinja2.select_autoescape(["html", "xml"])
+    )
+    
+    root_template = environment.get_template("root.html")
+    return root_template.render(domain=domain_finder.find_domain())
 
 
 if __name__ == '__main__':
